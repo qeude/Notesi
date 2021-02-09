b384224ad5f3256dde6482988b635af844eb36ee
Quentin Eude <quentineude@gmail.com>
2021-02-09 20:22:21 +0100

    Initial Commit

---
+++ Shared/NotesiApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesiApp.swift
+//  Shared
+//
+//  Created by Quentin Eude on 09/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct NotesiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
